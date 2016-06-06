2f9919db16c76c6280762739d8e3b951b140a6b7
firealarm <firealarm72@gmail.com>
2016-06-06 16:20:22 +0900

    assets

---
+++ RunningGame2/Assets/Script/GroundScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundScript : MonoBehaviour {
+    //Material texture offset rate
+    float speed = 1.2f;
+
+    //Offset the material texture at a constant rate
+    void Update()
+    {
+        float offset = Time.time * speed;
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(0, -offset);
+    }
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	
+}
